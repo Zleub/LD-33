6f34f7baee9bcf797bdce3edc85906585d0ab547
Zleub <debray.arnaud@gmail.com>
2015-08-22 01:03:43 +0200

    LOVE basics

---
+++ main.lua
@@ -0,0 +1,20 @@
+--          `--::-.`
+--      ./shddddddddhs+.
+--    :yddddddddddddddddy:
+--  `sdddddddddddddddddddds`
+--  ydddh+sdddddddddy+ydddds  main.lua
+-- /ddddy:oddddddddds:sddddd/ By adebray - adebray
+-- sdddddddddddddddddddddddds
+-- sdddddddddddddddddddddddds Created: 2015-08-22 01:00:10
+-- :ddddddddddhyyddddddddddd: Modified: 2015-08-22 01:02:08
+--  odddddddd/`:-`sdddddddds
+--   +ddddddh`+dh +dddddddo
+--    -sdddddh///sdddddds-
+--      .+ydddddddddhs/.
+--          .-::::-`
+
+function love.load() end
+
+function love.update(dt) end
+
+function love.draw() end
